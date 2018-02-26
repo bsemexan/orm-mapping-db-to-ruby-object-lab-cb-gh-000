7cf11aa6f2924768f266707bfccf3dde7283fc37
Berry  Semexan <bsemexan@gmail.com>
2018-02-26 21:20:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = ?
+      WHERE grade = 9
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
