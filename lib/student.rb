0b74659d5f936e6e97b8fe52d0386ad21f890761
Berry  Semexan <bsemexan@gmail.com>
2018-02-26 21:17:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,12 +34,11 @@ class Student
     end.first
   end
 
-  def self.count_all_students_in_grade_9(grade)
+  def self.count_all_students_in_grade_9
     sql = <<-SQL
       SELECT *
       FROM students
       WHERE grade = ?
-      LIMIT 1
     SQL
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
