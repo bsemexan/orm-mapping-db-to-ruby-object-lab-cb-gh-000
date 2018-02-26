8c603c9c98648384f39003393dd07216c2a1039a
Berry  Semexan <bsemexan@gmail.com>
2018-02-26 21:31:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,7 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT count(*)
       FROM students
