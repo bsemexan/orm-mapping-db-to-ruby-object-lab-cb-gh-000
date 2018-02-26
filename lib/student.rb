c631a4efc86dc2b0323942fb3aff27030251af70
Berry  Semexan <bsemexan@gmail.com>
2018-02-26 21:40:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,7 +68,7 @@ class Student
       SELECT *
       FROM students
     SQL
-    DB[:conn].execute(sql, grade).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
