0a3274fbae7800f7fce74a24657c5932291aef4f
Berry  Semexan <bsemexan@gmail.com>
2018-02-26 21:28:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,6 +52,15 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self..all_students_in_grade_X
+    sql = <<-SQL
+      SELECT count(*)
+      FROM students
+      WHERE grade < ?
+    SQL
+    DB[:conn].execute(sql)
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
