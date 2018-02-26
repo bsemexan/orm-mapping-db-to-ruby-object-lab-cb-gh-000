e91ca7d6e3df3f90a01ec2b74cc0df67b62bb82a
Berry  Semexan <bsemexan@gmail.com>
2018-02-26 21:54:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -90,7 +90,7 @@ class Student
       SELECT *
       FROM students
       WHERE grade = ?
-      LIMIT 1
+      LIMIT ?
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
